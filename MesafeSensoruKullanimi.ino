0770013f3eaedc2375b3cfa1de1ac373ae72e42d
Esat GÖZCÜ <35576161+esatgozcu@users.noreply.github.com>
2018-06-08 08:07:01 +0300

    Add files via upload

---
+++ MesafeSensoruKullanimi.ino
@@ -0,0 +1,26 @@
+const int trig_pin = 2;
+const int echo_pin = 3;
+
+int sure = 0;
+int mesafe = 0;
+
+void setup() {
+  pinMode(trig_pin,OUTPUT);
+  pinMode(echo_pin,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trig_pin,HIGH);
+  delay(1);
+  digitalWrite(trig_pin,LOW);
+
+  sure = pulseIn(echo_pin,HIGH);
+  
+  mesafe = (sure/2)/29.1;
+
+  Serial.print("Mesafe : ");
+  Serial.print(mesafe);
+  Serial.println(" cm");
+  delay(500);
+}
